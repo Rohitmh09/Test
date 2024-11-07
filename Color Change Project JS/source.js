04585fa0d8d1f841ee3e760ce21d1550c19644ae
Rohitmh09 <rohitmahadik787@gmail.com>
2024-11-07 15:52:21 +0530

    first upload

---
+++ Color Change Project JS/source.js
@@ -0,0 +1,40 @@
+let body = document.querySelector("body");
+let buttons = document.querySelectorAll(".button"); // provide array of buttons you can apply foreach
+console.log(buttons);
+
+buttons.forEach(function (button) {
+  
+    
+  button.addEventListener("click", function (event) 
+                                    {
+                                     console.log(event.target);// privide you all attrivutes of button class means its id also
+                                     let myId = event.target.id;
+                                     
+                                     switch (myId) {
+                                     case "gray":
+                                        body.style.backgroundColor = "gray";
+                                        break; 
+
+                                     case "pink":
+                                        body.style.backgroundColor = "pink";
+                                        break; 
+
+                                     case "red":
+                                        body.style.backgroundColor = "red";
+                                        break; 
+
+                                     case "white":
+                                        body.style.backgroundColor = "white";
+                                        break; 
+                                    }
+                                    })
+
+
+});
+
+const clock = document.querySelector("#clock");
+
+setInterval(function () {
+    let date = new Date();
+       clock.innerHTML = date.toLocaleTimeString();
+},1000);
